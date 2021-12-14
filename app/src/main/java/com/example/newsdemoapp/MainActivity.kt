2e7f75d74da5495ad2a9b0dc9191c0b85f44c498
Hélder Gonçalves <helderpimentaxc@gmail.com>
2021-12-14 17:16:48 +0000

    Final Project

--- app/src/main/java/com/example/newsdemoapp/MainActivity.kt
+++ app/src/main/java/com/example/newsdemoapp/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
-import android.view.View
-import android.widget.Toast
 import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.news_row.*
